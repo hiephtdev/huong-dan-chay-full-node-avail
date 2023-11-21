94765f68c007f05e8f8f58c2943e62a1aefc1e62
hiephtdev <100410288+hiephtdev@users.noreply.github.com>
2023-11-21 21:34:44 +0700

    Update auto-run-avail-full-node.sh

--- auto-run-avail-full-node.sh
+++ auto-run-avail-full-node.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo -e "\e[1;33m"
 echo "    __    __  __   __    _____   _______  _____    _____  __          __ _    _    ____  "
 echo "   |  \  /  |\  \ /  /  / ____| |__   __||_   _|  / ____| \ \        / /| |  | |  / __ \ "
@@ -6,7 +7,8 @@ echo "   |   \/   | \  V  /  | (___      | |     | |   | |       \ \  /\  / / | 
 echo "   | |\  /| |  \   /    \___ \     | |     | |   | |        \ \/  \/ /  |  __  | | |  | |"
 echo "   | | \/ | |   | |     ____) |    | |    _| |_  | |____     \  /\  /   | |  | | | |__| |"
 echo "   |_|    |_|   |_|    |_____/     |_|   |_____|  \_____|     \/  \/    |_|  |_|  \____/ "
-echo -e "\e[0m" sleep 2;
+echo -e "\e[0m"
+sleep 2;
 # set vars
 AVAIL_P2P_PORT=30333
 AVAIL_RPC_PORT=9944
@@ -75,10 +77,10 @@ sudo systemctl daemon-reload
 sudo systemctl enable availd
 sudo systemctl restart availd
 
-echo '=============== SETUP FINISHED ==================='
-echo -e "View the logs from the running service, use: journalctl -f -u availd.service"
-echo -e "Check if the node is running, enter: sudo systemctl status availd.service"
-echo -e "Stop your Avail node, use: sudo systemctl stop availd.service"
-echo -e "Start your Avail node, enter: sudo systemctl start availd.service"
-echo -e "After modifying the availd.service file, reload the service using: sudo systemctl daemon-reload"
-echo -e "Restart the service, use: sudo systemctl restart availd.service"
+echo -e "\e[1;33m=============== SETUP FINISHED ===================\e[0m"
+echo -e "\e[1;33mView the logs from the running service, use: journalctl -f -u availd.service\e[0m"
+echo -e "\e[1;33mCheck if the node is running, enter: sudo systemctl status availd.service\e[0m"
+echo -e "\e[1;33mStop your Avail node, use: sudo systemctl stop availd.service\e[0m"
+echo -e "\e[1;33mStart your Avail node, enter: sudo systemctl start availd.service\e[0m"
+echo -e "\e[1;33mAfter modifying the availd.service file, reload the service using: sudo systemctl daemon-reload\e[0m"
+echo -e "\e[1;33mRestart the service, use: sudo systemctl restart availd.service\e[0m"
