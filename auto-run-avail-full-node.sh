20d18f37491869d7b80ee3ea7ded73e768a0dd77
hiephtdev <100410288+hiephtdev@users.noreply.github.com>
2023-11-21 22:22:06 +0700

    Add files via upload

--- auto-run-avail-full-node.sh
+++ auto-run-avail-full-node.sh
@@ -11,10 +11,11 @@ echo -e "\e[0m"
 sleep 2;
 # set vars
 AVAIL_TAG="v1.8.0.2"
-echo -n "Enter node name: "
-	read AVAIL_NODE_NAME
+if [ ! $AVAIL_NODE_NAME ]; then
+	read -p "Enter node name: " AVAIL_NODE_NAME
 	echo 'export AVAIL_NODE_NAME='$AVAIL_NODE_NAME >> $HOME/.bash_profile
-source ~/.bash_profile
+fi
+source $HOME/.bash_profile
 echo -e '================================================='
 echo -e "Your node name: \e[1;33m$AVAIL_NODE_NAME\e[0m"
 echo -e "Your p2p port: \e[1;33m30333\e[0m"
