f86b57943af873986d10145f6e8e74a5ff7e0de2
hiephtdev <100410288+hiephtdev@users.noreply.github.com>
2023-11-25 21:40:14 +0700

    Add files via upload

--- auto-run-avail-light-node.sh
+++ auto-run-avail-light-node.sh
@@ -17,33 +17,12 @@ echo -e "\e[1;33m2. Installing dependencies... \e[0m" && sleep 1
 # packages
 sudo apt install curl tar wget clang pkg-config protobuf-compiler libssl-dev jq build-essential protobuf-compiler bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
 
-#variable gen secretkey
-NEW_SEED=$(date | sha256sum | base64 | head -c 32)
-
-# install rust
-if ! command -v rustup &> /dev/null
-then
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-	source ~/.cargo/env
-	rustup default stable
-	rustup update
-	rustup update nightly
-	rustup target add wasm32-unknown-unknown --toolchain nightly
-	sleep 1
-fi
-
-echo -e "\e[1;33m3. Downloading and building binaries... \e[0m" && sleep 1
+echo -e "\e[1;33m3. Downloading... \e[0m" && sleep 1
 # download binary
-git clone https://github.com/availproject/avail-light.git
-cd avail-light
-wget -O avail-config.yaml https://raw.githubusercontent.com/hiephtdev/huong-dan-chay-full-node-avail/main/avail-config.yaml
-# change secretkey
-sed -i.bak -e "s/seed={seed}/seed=$NEW_SEED/g" ./avail-config.yaml
-echo $NEW_SEED > secretkey.txt
-
-git checkout v1.7.4
-cargo build --release
-sudo cp $HOME/avail-light/target/release/avail-light /usr/local/bin
+cd $HOME
+mkdir avail-light
+wget https://github.com/availproject/avail-light/releases/download/v1.7.4/avail-light-linux-amd64.tar.gz
+tar -xvzf avail-light-linux-amd64.tar.gz
 # create service
 sudo tee /etc/systemd/system/availightd.service > /dev/null <<EOF
 [Unit]
@@ -52,7 +31,7 @@ After=network-online.target
 
 [Service]
 User=$USER
-ExecStart=$(which avail-light) --config $HOME/avail-light/config.yaml --network goldberg
+ExecStart=$HOME/avail-light/avail-light-linux-amd64 --network goldberg
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=65535
@@ -64,8 +43,8 @@ EOF
 echo -e "\e[1;33m4. Starting service... \e[0m" && sleep 1;
 # start service
 sudo systemctl daemon-reload
-sudo systemctl enable availd
-sudo systemctl restart availd
+sudo systemctl enable availightd
+sudo systemctl restart availightd
 
 echo -e "\e[1;33m=============== SETUP FINISHED ===================\e[0m"
 echo -e "\e[1;33mView the logs from the running service, use: journalctl -f -u availightd\e[0m"
